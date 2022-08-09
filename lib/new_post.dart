a74e55e6a52799891931e86e14652282213abbe4
Andrew Vo <andrew.van.vo@gmail.com>
2022-08-08 20:20:59 -0400

    coordinates loading

--- lib/new_post.dart
+++ lib/new_post.dart
@@ -2,7 +2,16 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:location/location.dart';
+
+class DataFields {
+  DateTime date = DateTime.now();
+  String? url;
+  int? amount;
+  String? coordinates;
+}
 
 class NewPost extends StatefulWidget {
   final Map arg;
@@ -14,20 +23,72 @@ class NewPost extends StatefulWidget {
 
 class _NewPostState extends State<NewPost> {
   File? imageLink;
-  Future grabImage() async {
-    imageLink = widget.arg['url'];
+  LocationData? locationData;
+  var locationService = Location();
+
+  @override
+  void initState() {
+    super.initState();
+    retrieveLocation();
+  }
+
+  void retrieveLocation() async {
+    try {
+      var _serviceEnabled = await locationService.serviceEnabled();
+      if (!_serviceEnabled) {
+        _serviceEnabled = await locationService.requestService();
+        if (!_serviceEnabled) {
+          print('Failed to enable service. Returning.');
+          return;
+        }
+      }
+
+      var _permissionGranted = await locationService.hasPermission();
+      if (_permissionGranted == PermissionStatus.denied) {
+        _permissionGranted = await locationService.requestPermission();
+        if (_permissionGranted != PermissionStatus.granted) {
+          print('Location service permission not granted. Returning.');
+        }
+      }
+
+      locationData = await locationService.getLocation();
+    } on PlatformException catch (e) {
+      print('Error: ${e.toString()}, code: ${e.code}');
+      locationData = null;
+    }
+    locationData = await locationService.getLocation();
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Center(
-          child: Image.file(widget.arg['image']),
-        )
-      ],
-    ));
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Latitude: ${locationData?.latitude}',
+          ),
+          Text(
+            'Longitude:  ${locationData?.longitude}',
+          ),
+          Center(
+            child: Image.file(widget.arg['image']),
+          ),
+          Center(
+              child: TextField(
+            keyboardType: TextInputType.number,
+            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+          ))
+        ],
+      ),
+
+      //floatingActionButton: FloatingActionButton(
+      //  //onPressed: (),
+      //  child: Icon(Icons.upload),
+      //),
+      //floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      //onTap:
+    );
   }
 }
